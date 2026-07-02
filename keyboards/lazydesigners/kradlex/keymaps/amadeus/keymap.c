ed69a413f7b18eaf28af796b718014256f079325
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-07-01 23:00:33 -0700

    Add the kradlex layout

---
+++ keyboards/lazydesigners/kradlex/keymaps/amadeus/keymap.c
@@ -0,0 +1,115 @@
+// Copyright 2026 Amadeus
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum {
+    TD_FLASH,
+};
+
+void alt_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 5) {
+        reset_keyboard();
+    }
+};
+
+tap_dance_action_t tap_dance_actions[] = {[TD_FLASH] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alt_finished, NULL)};
+
+enum kradlex_layers {
+  _BASE,
+  _LOWER,
+  _RAISE,
+  _ADJUST
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base
+     * ╭───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────╮
+     * │ Tab   │   Q   │   W   │   E   │   R   │   T   │   Y   │   U   │   I   │   O   │   P   │ Bksp  │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Ctrl  │   A   │   S   │   D   │   F   │   G   │   H   │   J   │   K   │   L   │   ;   │   '   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Shift │   Z   │   X   │   C   │   V   │   B   │   N   │   M   │   ,   │   .   │   /   │ Shift │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Esc   │ Ctrl  │ Alt   │ GUI   │ Lower │     Space     │ Raise │ GUI   │ Alt   │ Ctrl  │ Enter │
+     * ╰───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────╯
+     */
+    [_BASE] = LAYOUT(
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,
+        KC_RCTL,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+        KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  MO(1),  KC_SPC,  MO(2),   KC_RGUI, KC_RALT,  KC_RCTL,  KC_ENT
+    ),
+    /* Lower
+     * ╭───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────╮
+     * │   ~   │   !   │   @   │   #   │   $   │   %   │   ^   │   &   │   *   │   (   │   )   │   |   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │   `   │   1   │   2   │   3   │   4   │   5   │   6   │   7   │   8   │   9   │   0   │   \   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │   ,   │   <   │   >   │   =   │   -   │   _   │   +   │   {   │   }   │   [   │   ]   │   .   │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Esc   │ Ctrl  │ Alt   │ GUI   │ ----- │     Space     │ Adjst │ GUI   │ Alt   │ Ctrl  │ Enter │
+     * ╰───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────╯
+     */
+    [_LOWER] = LAYOUT(
+        KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_PIPE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,
+        KC_COMM,  KC_LT,    KC_GT,    KC_EQL,   KC_MINS,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,  KC_DOT,
+        KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_TRNS,  KC_SPC,   MO(3),    KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
+    ),
+    /* Raise
+     * ╭───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────╮
+     * │ Tab   │       │       │       │       │       │       │       │       │       │       │  Del  │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Ctrl  │ Menu  │ Home  │ PgUp  │ PgDn  │ End   │ Left  │ Down  │  Up   │ Right │       │       │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Shift │       │ Pause │ ScrLk │ Ins   │       │       │       │       │       │       │ Shift │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Esc   │ Ctrl  │ Alt   │ GUI   │ Adjst │     Space     │ ----- │ GUI   │ Alt   │ Ctrl  │ Enter │
+     * ╰───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────╯
+     */
+    [_RAISE] = LAYOUT(
+        KC_TAB,   KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_DEL,
+        KC_RCTL,  KC_MENU,  KC_HOME,   KC_PGUP,  KC_PGDN,  KC_END,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NO,    KC_NO,
+        KC_LSFT,  KC_NO,    KC_PAUSE,  KC_SCRL,  KC_INS,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_RSFT,
+        KC_ESC,   KC_LCTL,  KC_LALT,   KC_LGUI,  MO(3),    KC_SPC,  KC_TRNS,  KC_RGUI,  KC_RALT,  KC_RCTL,  KC_ENT
+    ),
+    /* Adjust
+     * ╭───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────╮
+     * │       │  F1   │  F2   │  F3   │  F4   │  F5   │  F6   │  F7   │  F8   │  F9   │  F10  │  F11  │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Ctrl  │       │       │ Bri+  │ Vol+  │ Play  │       │ Caps  │       │       │       │  F12  │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Shift │       │       │ Bri-  │ Vol-  │ Mute  │       │ Pscr  │       │       │       │ Shift │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
+     * │ Esc   │ Ctrl  │ Alt   │ GUI   │ ----- │     Space     │ ----- │ GUI   │ Alt   │ Ctrl  │ BLoad │
+     * ╰───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────╯
+     */
+    [_ADJUST] = LAYOUT(
+        KC_NO,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,     KC_F9,    KC_F10,   KC_F11,
+        KC_RCTL,  KC_NO,    KC_NO,    KC_BRIU,  KC_VOLU,  KC_MPLY,  KC_NO,    KC_CAPS,  KC_NO,     KC_NO,    KC_NO,    KC_F12,
+        KC_LSFT,  KC_NO,    KC_NO,    KC_BRID,  KC_VOLD,  KC_MUTE,  KC_NO,    KC_PSCR,  KC_NO,     KC_NO,    KC_NO,    KC_RSFT,
+        KC_ESC,   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_TRNS,  KC_SPC,   KC_TRNS,  KC_RGUI,  KC_RALT,   KC_RCTL,  TD(TD_FLASH)
+    ),
+};
+
+// Custom Caps Word implementation -- basically only shift letters, not
+// symbols, given my layout
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Capitalize all letter keys
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));
+            return true;
+        // Keycodes that still continue Caps Word
+        case KC_MINS:
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+        // Otherwise, stop Caps Word
+        default:
+            return false;
+    }
+}
